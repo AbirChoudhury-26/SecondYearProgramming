74585db5251c1f34e72fcdf05c21a5125512a288
AbirChoudhury-26 <abirudaipur@gmail.com>
2023-05-08 12:30:30 +0530

    New

---
+++ Second-Year_Practicals/OOPS/Exception-Handling.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void test(int x)
+{
+    try
+    {
+        if (x == 1)
+            throw x;
+        else if (x == 0)
+            throw 'x';
+        else if (x == -1)
+            throw 1.0;
+        cout << "End of try-block " << endl;
+    }
+    catch (char c)
+    {
+        cout << "Caught an character" << endl;
+    }
+    catch (int m)
+    {
+        cout << "Caught an integer" << endl;
+    }
+    catch (double d)
+    {
+        cout << "Caught an double" << endl;
+    }
+    cout << "End-of try catch system" << endl;
+}
+
+int main()
+{
+    cout << "Testing Multiple Catches " << endl;
+    cout << "x==1" << endl;
+    test(1);
+    cout << "x==0" << endl;
+    test(0);
+    cout << "x==-1" << endl;
+    test(-1);
+    cout << "x==2" << endl;
+    test(2);
+
+    return 0;
+}
